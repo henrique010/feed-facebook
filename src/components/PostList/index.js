07f89f6916a16300d63ae5657bdd8d9c67b96dd4
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2020-03-09 19:39:45 -0300

    adjustements to css added

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -59,6 +59,52 @@ class PostList extends Component {
             content: "Que massa! Estou pensando em me inscrever na próxima turma pra ver qual é desse bootcamp GoStack, dizem que os devs saem de lá com super poderes."
           },
         ]
+      },
+      {
+        id: 3,
+        author: {
+          name: "Julio Alcantara",
+          avatar: profile,
+        },
+        date: "04 Jun 2019",
+        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Diego Fernandes",
+              avatar: profile2,
+            },
+            content: "A Rocketseat está sempre em busca de novos membros para o time, inclusive ficamos de olho principalmente nos nossos alunos que se destacam no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp."
+          },
+        ]
+      },
+      {
+        id: 4,
+        author: {
+          name: "Gabriel Lisboa",
+          avatar: profile3,
+        },
+        date: "04 Jun 2019",
+        content: "Fala galera, beleza? \n Estou fazendo o bootacamp GoStack da Rocketseat e está sendo muito massa! Alguém mais ai fazendo, comenta na publicação para trocarmos uma idéia.",
+        comments: [
+          {
+            id: 1,
+            author: {
+              name: "Clara Lisboa",
+              avatar: profile4,
+            },
+            content: "Também estou fazendo o bootcamp e estou adorando! Estou no terceiro módulo sobre Node e já tenho minha api dos desafios costruída."
+          },
+          {
+            id: 2,
+            author: {
+              name: "Cézar Toledo",
+              avatar: profile5,
+            },
+            content: "Que massa! Estou pensando em me inscrever na próxima turma pra ver qual é desse bootcamp GoStack, dizem que os devs saem de lá com super poderes."
+          },
+        ]
       }
     ]
   }
